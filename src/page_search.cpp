c9ebfa0601fc1a9c739d3bb72319358ccf0873ea
crisis <61929733+SonglinLife@users.noreply.github.com>
2022-12-06 19:40:10 +0800

    Add IP and remove graph partition dim and npts specify (#15)
    Signed-off-by: SonglinLife <wusonglin@tongji.edu.cn>
    
    Signed-off-by: SonglinLife <wusonglin@tongji.edu.cn>

--- src/page_search.cpp
+++ src/page_search.cpp
@@ -55,7 +55,9 @@ namespace diskann {
     const T *    query = data.scratch.aligned_query_T;
     const float *query_float = data.scratch.aligned_query_float;
 
-    for (uint32_t i = 0; i < this->data_dim; i++) {
+    uint32_t query_dim = metric == diskann::Metric::INNER_PRODUCT ? this-> data_dim - 1: this-> data_dim;
+
+    for (uint32_t i = 0; i < query_dim; i++) {
       data.scratch.aligned_query_float[i] = query1[i];
       data.scratch.aligned_query_T[i] = query1[i];
       query_norm += query1[i] * query1[i];
